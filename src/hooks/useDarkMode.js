6a119ccb2850740b7c250e05463faa04888406eb
Elijah <elijahoneal@yahoo.com>
2021-03-16 22:22:54 -0400

    sretch goal

--- src/hooks/useDarkMode.js
+++ src/hooks/useDarkMode.js
@@ -1,8 +1,32 @@
-import {useState} from 'react'
+import {useEffect} from 'react'
 import useLocalStorage from './useLocalStorage'
+import useMedia from './useMedia'
 const useDarkMode = () =>{
+
+    const usePrefersDarkMode = () => {
+        return useMedia(['prefers-color-scheme: dark'], [true], false)
+    }
     const [darkMode , setDarkMode] = useLocalStorage('dark mode')
-    return [darkMode , setDarkMode]
+
+    const prefersDarkMode = usePrefersDarkMode();
+    const enabled = typeof darkMode !== 'undefined' ? darkMode : prefersDarkMode;
+    useEffect(
+        () => {
+          const className = 'dark-mode';
+          const element = window.document.body;
+          if (enabled) {
+            element.classList.add(className);
+          } else {
+            element.classList.remove(className);
+          }
+        },
+        [enabled] // Only re-call effect when value changes
+      );
+    
+      // Return enabled state and setter
+    
+    
+    return [enabled , setDarkMode]
 }
 
 export default useDarkMode
